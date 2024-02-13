d3c0df7598c301142e061e15206a6877bb9e6a53
ANUJA\anuja <anujachirantha@gmail.com>
2024-02-13 19:02:35 +0530

    add routing

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -19,12 +19,16 @@ export class ShopService {
 
     if (shopParams.brandId) params = params.append('brandId', shopParams.brandId);
     if (shopParams.typeId) params = params.append('typeId', shopParams.typeId);
-       params = params.append('sort', shopParams.sort);
-       params = params.append('pageIndex', shopParams.pageNumber);
-       params = params.append('pageSize', shopParams.pageSize);
-    if (shopParams.search)  params = params.append('search', shopParams.search);
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
+    if (shopParams.search) params = params.append('search', shopParams.search);
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', { params });
+  }
+
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {

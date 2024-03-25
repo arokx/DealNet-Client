90ac0e3b7ad5d210cc9259117466d955ddcc6d49
ANUJA\anuja <anujachirantha@gmail.com>
2024-03-25 06:40:37 +0530

    checkout

---
+++ src/app/checkout/checkout-address/checkout-address.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
+
+@Component({
+  selector: 'app-checkout-address',
+  templateUrl: './checkout-address.component.html',
+  styleUrls: ['./checkout-address.component.scss']
+})
+export class CheckoutAddressComponent {
+  @Input() checkoutForm?: FormGroup;
+
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+}
